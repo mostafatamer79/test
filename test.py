2bd265f8bc7e2df866b572a34da51fe39c93d4e0
mostafatam79 <105394598+mostafatamer79@users.noreply.github.com>
2024-09-20 03:19:21 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,18 +2,7 @@ import streamlit as st
 
 
 def show_requirements():
-    st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+    
     requirements = """
     streamlits
     selenium
@@ -30,7 +19,18 @@ def show_requirements():
     st.text_area("requirements.txt", requirements, height=400)
 
 def main():
-  
+    st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
     show_requirements()
     
 if __name__ == "__main__":
