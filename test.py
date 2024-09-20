cf29a6650c47fe88ec20d3cc824b2fc1d8ded083
mostafatam79 <105394598+mostafatamer79@users.noreply.github.com>
2024-09-20 03:32:51 +0300

    Update test.py

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ def main():
     hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
+            .viewerBadge_container__r5tak {visibility: hidden;}
             </style>
             """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
