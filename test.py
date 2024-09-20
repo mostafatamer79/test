b176f274e7b6959c94117a556c9ee026160f5c00
mostafatam79 <105394598+mostafatamer79@users.noreply.github.com>
2024-09-20 03:02:02 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
-from Auth.auth import auth
-from Auth.logout import logout
-from icon.icon import icon
-
 
 def show_requirements():
     requirements = """
@@ -22,9 +18,7 @@ def show_requirements():
     st.text_area("requirements.txt", requirements, height=400)
 
 def main():
-    icon()
-    auth()
-    logout()
+  
     show_requirements()
     
 if __name__ == "__main__":
