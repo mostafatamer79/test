0db5a98d65113923c733050800b1715cfa06dc0d
mostafatam79 <105394598+mostafatamer79@users.noreply.github.com>
2024-09-20 03:25:18 +0300

    Update test.py

--- test.py
+++ test.py
@@ -19,18 +19,13 @@ def show_requirements():
     st.text_area("requirements.txt", requirements, height=400)
 
 def main():
-    st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .stHeader,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+    hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
     show_requirements()
     
 if __name__ == "__main__":
